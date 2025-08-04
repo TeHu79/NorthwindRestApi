050cbe421e9b19547608b617ff2c30345812ff0c
Teija Hurme <teija.hurme@student.careeria.fi>
2025-08-04 20:54:34 +0300

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -12,12 +12,60 @@ namespace NorthwindRestApi.Controllers
         //Alustetaan tietokantayhteys
         Northwind1Context db = new Northwind1Context();
 
-        //Hakee kaikkia asiakkaat
+        //Hakee kaikki asiakkaat
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch(Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
         }
+
+
+        //Hakee yhden asiakkaan pääavaimella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    //return BadRequest("Asiakasta id:llä " + id + " ei löydy.");//perinteinen tapa liittää muuttuja
+                    return NotFound($"Asiakasta id:llä {id} ei löydy."); // string interpolation -tapa
+                }           
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e);
+            }
+        }
+
+        //Uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust) 
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
     }
 }
