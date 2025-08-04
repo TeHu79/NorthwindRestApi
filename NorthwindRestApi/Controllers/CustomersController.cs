4ac498ecbe4d037259dd98995677647a1db8c0c3
Teija Hurme <teija.hurme@student.careeria.fi>
2025-08-04 20:22:20 +0300

    Add project files.

---
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models; 
+
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        //Alustetaan tietokantayhteys
+        Northwind1Context db = new Northwind1Context();
+
+        //Hakee kaikkia asiakkaat
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        {
+            var asiakkaat = db.Customers.ToList();
+            return Ok(asiakkaat);
+        }
+    }
+}
